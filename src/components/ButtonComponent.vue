ff633cc801d7bb4573a4ee7207d0a66d80c322d2
ChristianBielak <christian.bielak-extern@deutschebahn.com>
2022-10-21 09:39:43 +0200

    added button component

---
+++ src/components/ButtonComponent.vue
@@ -0,0 +1,22 @@
+<template>
+  <button @click="emitEvent()">
+    {{ text }}
+  </button>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop, Emit } from "vue-property-decorator";
+
+@Component
+export default class extends Vue {
+  @Prop()
+  text!: string;
+
+  @Emit("buttonClicked")
+  emitEvent() {
+    // just emit event
+  }
+}
+</script>
+
+<style scoped></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,25 +1,42 @@
 <template>
   <div class="home">
-    {{ $todos }}
+    {{ todos }}
+    <ButtonComponent text="click me" @buttonClicked="addTodo('foo')" />
+    <p>
+      {{ counter }}
+    </p>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from "vue-property-decorator";
+import { Component, Vue, Watch } from "vue-property-decorator";
 import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import ButtonComponent from "@/components/ButtonComponent.vue"; // @ is an alias to /src
 
 @Component({
   components: {
     HelloWorld,
+    ButtonComponent,
   },
 })
 export default class HomeView extends Vue {
+  counter = 0;
+
   addTodo(todo: string) {
     this.$store.dispatch("todo/addTodo", todo);
   }
 
   get activeTodo(): string {
     return this.$store.getters["todo/getTodo"](1);
+  }
+
+  get todos(): string[] {
+    return this.$store.state.todo.todos;
+  }
+
+  @Watch("todos")
+  onTodosChange() {
+    this.counter++;
   }
 }
 </script>
